fb5791ea9e17016005e3e444dfe3a4410225c906
mykhaito <163899158+mykhaito@users.noreply.github.com>
2025-03-01 20:16:23 +0100

    some

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,41 @@
+import { useState, useEffect } from "react";
 import "../App/App.css";
-import userData from "../../json/userData.json";
-import friends from "../../json/friends.json"
-import items from "../../json/items.json"
 
-import Profile from "../Profile/Profile.jsx";
-import FriendList from "../FriendList/FriendList.jsx";
-import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx";
+import Description from "../Description/Description.jsx";
+import Options from "../Options/Options.jsx";
+import Feedback from "../Feedback/Feedback.jsx";
+import Notification from "../Notification/Notification.jsx";
 
 export default function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const roundFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType == "reset") {
+      setFeedback({ good: 0, neutral: 0, bad: 0 });
+      localStorage.removeItem("feedback");
+      return;
+    }
+    setFeedback((values) => ({
+      ...values,
+      [feedbackType]: values[feedbackType] + 1,
+    }));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
   return (
     <>
-      <Profile username={userData.username} tag={userData.tag} location={userData.location} avatar={userData.avatar} stats={userData.stats} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={items} />
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? <Feedback feedback={feedback} totalFeedback={totalFeedback} roundFeedback={roundFeedback} /> : <Notification />}
     </>
   );
 }
